5946a659129ffb465bf13d90e2a3a3078a4b0f0a
KowkaVN <41840441+KowkaVaweNorm@users.noreply.github.com>
2023-05-28 02:38:54 +0300

    add jest and first test

---
+++ src/shared/lib/ClassNames/ClassNames.test.ts
@@ -0,0 +1,34 @@
+import { classNames } from './ClassNames'
+
+describe('classNames', () => {
+  test('with only first param', () => {
+    expect(classNames('someClass')).toBe('someClass')
+  })
+
+  test('with only first param', () => {
+    expect(classNames('someClass', {}, ['class1'])).toBe('someClass class1')
+  })
+
+  test('with mods', () => {
+    expect(classNames(
+      'someClass',
+      { hovered: true, scrollable: true }
+    )).toBe('someClass hovered scrollable')
+  })
+
+  test('with mods false', () => {
+    expect(classNames(
+      'someClass',
+      { hovered: true, scrollable: false },
+      ['class1']
+    )).toBe('someClass hovered class1')
+  })
+
+  test('with mods undefined', () => {
+    expect(classNames(
+      'someClass',
+      { hovered: true, scrollable: undefined },
+      ['class1']
+    )).toBe('someClass hovered class1')
+  })
+})
--- src/shared/lib/ClassNames/ClassNames.ts
+++ src/shared/lib/ClassNames/ClassNames.ts
@@ -4,10 +4,10 @@ type Mods = Record<string, boolean | string>
 export function classNames (cls: string, mods: Mods = {}, additioonal: string[] = []): string {
   return [
     cls,
-    ...additioonal.filter(Boolean),
     ...Object.entries(mods)
       .filter(([, value]) => Boolean(value))
-      .map(([className, value]) => className)
+      .map(([className, value]) => className),
+    ...additioonal.filter(Boolean)
   ].join(' ')
 }
 
