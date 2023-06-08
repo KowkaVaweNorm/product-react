d3a520057b3be3b0950a742b7f0460d379975da1
KowkaVN <41840441+KowkaVaweNorm@users.noreply.github.com>
2023-06-09 01:20:36 +0300

    fix lost-pixel, add test.yml, update.yml

--- config/lost-pixel/lost-pixel.config.ts
+++ config/lost-pixel/lost-pixel.config.ts
@@ -3,6 +3,13 @@ export const config: CustomProjectConfig = {
   storybookShots: {
     storybookUrl: './storybook-static',
   },
+  pageShots: {
+    pages: [
+      {path: "/", name: 'main'},
+      {path: "/about", name: 'about'}
+    ],
+    baseUrl: "http://localhost:6006"
+  },
   generateOnly: true,
   failOnDifference: true,
 };
