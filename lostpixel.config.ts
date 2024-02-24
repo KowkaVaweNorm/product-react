aab8eee030468b570ed59679e7298165ab77a137
KowkaVN <kowka25122000@gmail.com>
2024-02-24 15:52:28 +0300

    fix lostPixel

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -4,7 +4,7 @@ import { CustomProjectConfig } from 'lost-pixel';
       storybookShots: {
         storybookUrl: './storybook-static',
       },
-    
+      waitBeforeScreenshot: 2000,
       lostPixelProjectId: 'clip35ifygwtdn70eekz2xjoj',
       apiKey: process.env.LOST_PIXEL_API_KEY,
     };
