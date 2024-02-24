c5a22023cf5a8a587cd9fa9fba6e3a1b665c8742
KowkaVN <kowka25122000@gmail.com>
2024-02-24 23:19:59 +0300

    fix api instance

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,8 +4,12 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 // const baseUrl = __IS_DEV__ ? 'http://localhost:8000' : '';
 
 export const $api = axios.create({
-  baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY)
+  baseURL: __API__
+});
+
+$api.interceptors.request.use((config) => {
+  if (config.headers != null) {
+    config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) ?? '';
   }
+  return config;
 });
