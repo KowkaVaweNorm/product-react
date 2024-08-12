0d01db6ad9a814b2c7e5e979d0488aabe8b60c2a
MichailWork <michail25@yandex.com>
2024-08-12 14:16:14 +0300

    обновил сигнатуру BuildSelector

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -1,12 +1,21 @@
 import { useSelector } from 'react-redux';
 import { type StateSchema } from '@/app/providers/StoreProvider';
 
-type Selector<T> = (state: StateSchema) => T;
-type Result<T> = [() => T, Selector<T>];
+type Selector<T, Args extends any[]> = (state: StateSchema, ...args: Args) => T;
+type Hook<T, Args extends any[]> = (...args: Args) => T;
+type Result<T, Args extends any[]> = [Hook<T, Args>, Selector<T, Args>];
 
-export function buildSelector<T>(selector: Selector<T>): Result<T> {
-  const useSelectorHook = () => {
-    return useSelector(selector);
+/**
+ * @example
+ * - 'selectors.ts'
+ * const [useArticleItemById] = buildSelector((state, id)=> state.article[id])
+ *
+ * - 'component.tsx'
+ * const article = useArticleItemById(id)
+ */
+export function buildSelector<T, Args extends any[]>(selector: Selector<T, Args>): Result<T, Args> {
+  const useSelectorHook: Hook<T, Args> = (...args: Args) => {
+    return useSelector((state: StateSchema) => selector(state, ...args));
   };
 
   return [useSelectorHook, selector];
